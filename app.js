aa9c883f7f37f2e60cf58cf964f948c8b30d9a6b
MaxSanders-work <maximiliano.sanders@mail.udp.cl>
2022-10-17 14:43:01 -0300

    Front

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+
+
+
+const app = express();
+const PORT = 3000;
+app.set('view engine', 'ejs');
+
+
+// INDEX
+app.get('/', (req,res)=>{
+  res.render('pages/index.ejs');
+});
+
+// CREAR UNA RESEÑA
+app.get('/createReview', (req,res)=>{
+  res.render('pages/createReview.ejs');
+});
+// PROCESO DE CREACION
+app.post('/createReview', (req,res)=>{
+  
+});
+
+// VER UNA RESEÑA
+app.get('/review', (req,res)=>{
+  res.render('pages/review.ejs');
+});
+
+// ELIMINAR UNA RECETA
+app.get('/deleteReview', (req,res)=>{
+  res.render('pages/deleteReview.ejs');
+});
+
+// ACTUALIZAR UNA RECETA
+app.get('/updateReview', (req,res)=>{
+  res.render('pages/updateReview.ejs');
+});
+
+
+
+app.listen(PORT, (error) =>{
+	if(!error)
+		console.log("Server is Successfully Running and App is listening on port "+ PORT)
+	else
+		console.log("Error occurred, server can't start", error);
+	}
+);
